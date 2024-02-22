a8026c40a8de6a6bfed2e5636304e266b8e38dd9
afanfian <afanfian14@gmail.com>
2024-02-22 21:12:47 +0700

    feat: change url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,16 @@
             <p class="text-4xl font-bold">Welcome to Steradian</p>
             <p class="text-lg md:px-40 py-5">Founded in 2016, Steradian Data Optima focus in deliver data-driven digital transformation to any size of organizations. Using proven technologies, platform and experienced engineer, we keep pursuing our purpose to make Artificial Intelligence accessible for everyone, either a big company looking for innovation or small business looking for exceptional growth, so they take a competitive advantage.</p>
             <p class="text-lg">Please go to the cars and orders dashboard page</p>
-            <a href="/cars" class="pl-1 pt-1 text-4xl"><i class="fas fa-chevron-circle-down cursor-pointer text-blue-500 hover:text-blue-600"></i></a>
-            <a href="/orders" class="pl-1 pt-1 text-4xl"><i class="fas fa-chevron-circle-down cursor-pointer text-red-500 hover:text-red-600"></i></a>
+            <div class="flex justify-center gap-10">
+                <div>
+                    <p>Mobil</p>
+                    <a href="/cars" class="pl-1 pt-1 text-4xl"><i class="fas fa-chevron-circle-down cursor-pointer text-blue-500 hover:text-blue-600"></i></a>
+                </div>
+                <div>
+                    <p>Order</p>
+                    <a href="/orders" class="pl-1 pt-1 text-4xl"><i class="fas fa-chevron-circle-down cursor-pointer text-red-500 hover:text-red-600"></i></a>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
